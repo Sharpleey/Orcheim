0292785ec91cd345128500e30eacba733b5f2670
Maxim Kirichenko <maksimkirichenko98@gmail.com>
2022-11-08 21:07:28 +0300

    Добавил на противников коллайдер со слоем Enemy, для взаимводействия с RaycastSphere - Сделал тестовый скрипт  TEST этой механики

---
+++ Assets/Scipts/TEST.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST : MonoBehaviour
+{
+    [SerializeField] LayerMask collisionMask;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            ExplosionDamage();
+        }
+    }
+
+    void ExplosionDamage()
+    {
+        Vector3 center = transform.position;
+        float radius = GetComponent<SphereCollider>().radius;
+
+        Collider[] hitColliders = Physics.OverlapSphere(center, radius, collisionMask);
+        foreach (var hitCollider in hitColliders)
+        {
+            Enemy enemy = hitCollider.GetComponent<Enemy>();
+            enemy.SetState<PursuitState>();
+        }
+    }
+}
