509324b04395b348ca4fae0738a7735b86f5c560
Maxim Kirichenko <maksimkirichenko98@gmail.com>
2023-01-28 16:55:18 +0300

    Добавление событий в PlayerEventManager - Добавлено событие изменения здоровья игрока - Добавлено событие изменения брони игрока - Добавлено событие изменения золота игрока - Добавлено событие изменения опыта игрока - Добавлено событие изменения уровня игрока

--- Assets/Scipts/EventManagers/PlayerEventManager.cs
+++ Assets/Scipts/EventManagers/PlayerEventManager.cs
@@ -17,6 +17,16 @@ public class PlayerEventManager
     /// Событие повышения лвл игрока
     /// </summary>
     public static readonly UnityEvent<int> OnPlayerLevelUp = new UnityEvent<int>();
+
+    public static readonly UnityEvent OnPlayerHealthChanged = new UnityEvent();
+
+    public static readonly UnityEvent OnPlayerArmorChanged = new UnityEvent();
+
+    public static readonly UnityEvent OnPlayerGoldChanged = new UnityEvent();
+
+    public static readonly UnityEvent OnPlayerExperienceChanged = new UnityEvent();
+
+    public static readonly UnityEvent OnPlayerLevelChanged = new UnityEvent();
     #endregion
 
     #region Methods
@@ -43,6 +53,46 @@ public class PlayerEventManager
     public static void PlayerLevelUp(int level)
     {
         OnPlayerLevelUp.Invoke(level);
+    }
+
+    /// <summary>
+    /// Вызов события изменения здоровья игрока
+    /// </summary>
+    public static void PlayerHealthChanged()
+    {
+        OnPlayerHealthChanged.Invoke();
+    }
+
+    /// <summary>
+    /// Вызов события изменения брони игрока
+    /// </summary>
+    public static void PlayerArmorChanged()
+    {
+        OnPlayerArmorChanged.Invoke();
+    }
+
+    /// <summary>
+    /// Вызов события изменения золота игрока
+    /// </summary>
+    public static void PlayerGoldChanged()
+    {
+        OnPlayerGoldChanged.Invoke();
+    }
+
+    /// <summary>
+    /// Вызов события изменения опыта игрока
+    /// </summary>
+    public static void PlayerExperienceChanged()
+    {
+        OnPlayerExperienceChanged.Invoke();
+    }
+
+    /// <summary>
+    /// Вызов события изменения уровня игрока
+    /// </summary>
+    public static void PlayerLevelUp()
+    {
+        OnPlayerLevelChanged.Invoke();
     }
     #endregion
 }
