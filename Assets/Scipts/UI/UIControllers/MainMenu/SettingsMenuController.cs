bed4150dff3e52955e34e3becd2f03cebd396423
Maxim Kirichenko <maksimkirichenko98@gmail.com>
2023-03-11 14:46:15 +0300

    Доавлена возможность настраивать качество графики в менгю настроек

--- Assets/Scipts/UI/UIControllers/MainMenu/SettingsMenuController.cs
+++ Assets/Scipts/UI/UIControllers/MainMenu/SettingsMenuController.cs
@@ -1,9 +1,14 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using TMPro;
 
 public class SettingsMenuController : MonoBehaviour
 {
+    #region Serialize fields
+
+    [Header("Button")]
     [SerializeField] private Button _buttonGameSettings;
     [SerializeField] private GameObject _subMenuGameSettings;
 
@@ -23,17 +28,49 @@ public class SettingsMenuController : MonoBehaviour
     [SerializeField] private Button _buttonOtherSettings;
     [SerializeField] private GameObject _subMenuOtherSettings;
 
-    [Header("Button")]
+    [Space(5)]
     [SerializeField] private Color _colorSelectable;
     [SerializeField] private Color _colorNormal;
 
     [Header("Audio Settings")]
     [SerializeField] private AudioMixer _generalAudioMixer;
+    [Space(5)]
+    [SerializeField] private Slider _sliderGeneralVolume;
+    [SerializeField] private Slider _sliderMainMenuMusicVolume;
+    [SerializeField] private Slider _sliderMusicVolume;
+    [SerializeField] private Slider _sliderAmbientVolume;
+    [SerializeField] private Slider _sliderSFXVolume;
+    [SerializeField] private Slider _sliderNPCVolume;
+
+    [Header("Video Settings")]
+    [SerializeField] private TMP_Dropdown _dropdownScreenResolution;
+    [SerializeField] private TMP_Dropdown _dropdownGraphicQuality;
+    [Space(5)]
+    [SerializeField] private Toggle _toggleFullscreen;
+
+    #endregion Serialize fields
+
+    #region Private fields
+
+    private MainMenuCanvasController _mainMenuCanvasController;
 
     private GameObject _activeSubMenu;
     private Button _activeButton;
 
-    private MainMenuCanvasController _mainMenuCanvasController;
+    private Resolution[] _resolutions;
+
+    #endregion Private fields
+
+    #region Mono
+
+    private void Awake()
+    {
+        InitParametersDropdownScreenResolution();
+        InitParametersToggleFullscreen();
+        InitParametersDropdownGraphicQuality();
+        InitParametersAudioSliders();
+    }
+
     private void Start()
     {
         _mainMenuCanvasController = GetComponentInParent<MainMenuCanvasController>();
@@ -45,6 +82,10 @@ public class SettingsMenuController : MonoBehaviour
         DisableAllSubMenu();
         ShowMenu(_subMenuAudioSettings, _buttonAudioSettings);
     }
+
+    #endregion Mono
+
+    #region Private methods
 
     private void ShowMenu(GameObject menu, Button button)
     {
@@ -89,7 +130,69 @@ public class SettingsMenuController : MonoBehaviour
         _subMenuOtherSettings.SetActive(false);
     }
 
-    #region OnClick
+    private void InitParametersDropdownScreenResolution()
+    {
+        int currentResolutionIndex = 0;
+        List<string> optionsDropdown = new List<string>();
+
+        _resolutions = Screen.resolutions;
+
+        _dropdownScreenResolution.ClearOptions();
+
+        for (int i = 0; i < _resolutions.Length; i++)
+        {
+            string option = $"{_resolutions[i].width}x{_resolutions[i].height}";
+
+            optionsDropdown.Add(option);
+
+            if (_resolutions[i].width == Screen.currentResolution.width && _resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        _dropdownScreenResolution.AddOptions(optionsDropdown);
+        _dropdownScreenResolution.value = currentResolutionIndex;
+        _dropdownScreenResolution.RefreshShownValue();
+    }
+
+    private void InitParametersDropdownGraphicQuality()
+    {
+        int currentQualityIndex = QualitySettings.GetQualityLevel();
+
+        _dropdownGraphicQuality.value = currentQualityIndex;
+    }
+
+    private void InitParametersToggleFullscreen()
+    {
+        _toggleFullscreen.isOn = Screen.fullScreen;
+    }
+
+    private void InitParametersAudioSliders()
+    {
+        if (_generalAudioMixer.GetFloat("GeneralVolume", out float valueGeneralVolume))
+            _sliderGeneralVolume.value = valueGeneralVolume;
+
+        if (_generalAudioMixer.GetFloat("MainMenuMusicVolume", out float valueMainMenuMusicVolume))
+            _sliderMainMenuMusicVolume.value = valueMainMenuMusicVolume;
+
+        if (_generalAudioMixer.GetFloat("MusicVolume", out float valueMusicVolume))
+            _sliderMusicVolume.value = valueMusicVolume;
+
+        if (_generalAudioMixer.GetFloat("AmbientVolume", out float valueAmbientVolume))
+            _sliderAmbientVolume.value = valueAmbientVolume;
+
+        if (_generalAudioMixer.GetFloat("SFXVolume", out float valueSFXVolume))
+            _sliderSFXVolume.value = valueSFXVolume;
+
+        if (_generalAudioMixer.GetFloat("NPCVolume", out float valueNPCVolume))
+            _sliderNPCVolume.value = valueNPCVolume;
+
+    }
+
+    #endregion Private methods
+
+    #region Button event handlers
 
     public void OnClickGameSettings()
     {
@@ -121,9 +224,9 @@ public class SettingsMenuController : MonoBehaviour
         _mainMenuCanvasController?.BackToMainMenu();
     }
 
-    #endregion OnClick
+    #endregion Button event handlers
 
-    #region Volume Sliders
+    #region Audio handlers
 
     public void SetGeneralVolume(float volume)
     {
@@ -151,5 +254,25 @@ public class SettingsMenuController : MonoBehaviour
         _generalAudioMixer.SetFloat("NPCVolume", volume);
     }
 
-    #endregion Volume Sliders
+    #endregion Audio handlers
+
+    #region Video handlers
+    public void SetGraphicQuality(int indexQuality)
+    {
+        QualitySettings.SetQualityLevel(indexQuality);
+    }
+
+    public void SetFullscreen(bool isFullsceen)
+    {
+        Screen.fullScreen = isFullsceen;
+    }
+
+    public void SetScreenResolution(int screenResolutionIndex)
+    {
+        Resolution resolution = _resolutions[screenResolutionIndex];
+
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    #endregion Video handlers
 }
