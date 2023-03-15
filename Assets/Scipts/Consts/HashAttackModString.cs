4d7d51f60cad4527954c9ffd2c7fef0439ade2fc
Maxim Kirichenko <maksimkirichenko98@gmail.com>
2023-03-15 18:05:46 +0300

    Добавил описание модификаторов атак в меню Learn

--- Assets/Scipts/Consts/HashAttackModString.cs
+++ Assets/Scipts/Consts/HashAttackModString.cs
@@ -15,7 +15,7 @@ public class HashAttackModString
     public const string SLOW_ATTACK_CHANCE_UPGRADE_DESCRIPTION = "Шанс наложить эффект +{0}% (Текущий {1}%)";
 
     public const string PENETRATION_PROJECTILE_NAME = "Пробивающий снаряд";
-    public const string PENETRATION_PROJECTILE_DESCRIPTION = "Снаряд пробивает несколько ({0}) целей и наносит урон уменьщающийся на {1}% с каждым пробитием";
+    public const string PENETRATION_PROJECTILE_DESCRIPTION = "Снаряд оружия дальнего боя пробивает несколько целей ({0}) и наносит урон уменьщающийся на {1}% с каждым пробитием";
     public const string PENETRATION_PROJECTILE_MAX_COUNT_UPGRADE_DESCRIPTION = "Число пробиваемые целей +{0} (Текущее значение: {1})";
     public const string PENETRATION_DAMAGE_DECREASE_COUNT_UPGRADE_DESCRIPTION = "Уменьшение урона с каждым пробитием {0}% (Текущее уменьшение {1}%)";
 }
